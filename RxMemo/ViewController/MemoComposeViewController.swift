fd693faafe105839cc88f7b039fcf7ae3e41973c
정민기 <chungminki@zuminternet.com>
2020-01-13 11:20:42 +0900

    add view controller class

---
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoComposeViewController.swift
+//  RxMemo
+//
+//  Created by devming on 2020/01/13.
+//  Copyright © 2020 devming. All rights reserved.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
