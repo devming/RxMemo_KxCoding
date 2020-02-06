bcfc28101aa272e68a9da1539cb5c9b4994134c0
devming <cmk330@gmail.com>
2020-02-06 16:58:17 +0900

    Step24. Keyboard notification + rx

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -40,6 +40,35 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
             .withLatestFrom(contentTextView.rx.text.orEmpty)
             .bind(to: viewModel.saveAction.inputs)
             .disposed(by: rx.disposeBag)
+        
+        /// TextView Keyboard 조절
+        ///
+        /// 해당 notification이 전달될 때 마다 새로운 next이벤트를 방출하는 옵저버블을 리턴한다.
+        let willShowObservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+            .map { ($0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height ?? 0 }
+        
+        let willHideObservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+            .map { noti -> CGFloat in 0 }
+        
+        /// 두 옵저버블 merge
+        
+        let keyboardObservable = Observable.merge(willShowObservable, willHideObservable).share()
+        
+        keyboardObservable
+            .subscribe(onNext: { [weak self] height in
+                guard let `self` = self else { return }
+                
+                var inset = self.contentTextView.contentInset
+                inset.bottom = height
+                
+                var scrollInset = self.contentTextView.scrollIndicatorInsets
+                scrollInset.bottom = height
+                
+                UIView.animate(withDuration: 0.3) {
+                    self.contentTextView.contentInset = inset
+                    self.contentTextView.scrollIndicatorInsets = scrollInset
+                }
+            }).disposed(by: rx.disposeBag)
     }
     
     override func viewWillAppear(_ animated: Bool) {
